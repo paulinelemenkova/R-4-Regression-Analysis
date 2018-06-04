4ff4b66d50f673a718b30db7444b9b8170f016a1
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2018-06-04 21:09:38 +0800

    Add files via upload

--- Regression_Profile18.R
+++ Regression_Profile18.R
@@ -1,53 +1,23 @@
-# QQ t.test
-# шаг-1. вчитываем таблицу. делаем из нее датафрейм. говорим сколько колонок и строк.
+# Regression Analysis
+############## ЧАСТЬ 1: делаем data.frame
+# шаг-1. вчитываем таблицу. делаем из нее датафрейм.
 MDepths <- read.csv("Depths.csv", header=TRUE, sep = ",")
-#MDepth<- data.frame(MDepths, nrow = 1008, ncol = 25)
 
 # шаг-2. чистим датафрейм от NA значений
-df<- na.omit(MDepths) 
-row.has.na <- apply(df, 1, function(x){any(is.na(x))}) # проверяем, удалил ли все NA
+MDF <- na.omit(MDepths) 
+row.has.na <- apply(MDF, 1, function(x){any(is.na(x))}) # проверяем, удалил ли все NA
 sum(row.has.na) # суммируем все NA, должно получиться: [1] 0
-head(df) # смотрим очищенный датафрейм. теперь с ним работаем.
+head(MDF) # смотрим очищенный датафрейм. теперь с ним работаем.
 
-#шаг-3. создаем ячейки датафрейма, вчитываем туда столбцы-колонки из таблицы
-nr_point<- as.numeric(df$observ)
-profile1 <- as.numeric(df$profile1)
-profile2 <- as.numeric(df$profile2)
-profile3 <- as.numeric(df$profile3)
-profile4 <- as.numeric(df$profile4)
-profile5 <- as.numeric(df$profile5)
-profile6 <- as.numeric(df$profile6)
-profile7 <- as.numeric(df$profile7)
-profile8 <- as.numeric(df$profile8)
-profile9 <- as.numeric(df$profile9)
-profile10 <- as.numeric(df$profile10)
-profile11 <- as.numeric(df$profile11)
-profile12 <- as.numeric(df$profile12)
-profile13 <- as.numeric(df$profile13)
-profile14 <- as.numeric(df$profile14)
-profile15 <- as.numeric(df$profile15)
-profile16 <- as.numeric(df$profile16)
-profile17 <- as.numeric(df$profile17)
-profile18 <- as.numeric(df$profile18)
-profile19 <- as.numeric(df$profile19)
-profile20 <- as.numeric(df$profile20)
-profile21 <- as.numeric(df$profile21)
-profile22 <- as.numeric(df$profile22)
-profile23 <- as.numeric(df$profile23)
-profile24 <- as.numeric(df$profile24)
-profile25 <- as.numeric(df$profile25)
+############## ЧАСТЬ 2: делаем регрессионный анализ и рисуем его график. (Мат. метод вкл. 3 вида кривой лесса с доверительными интервалами и квантилями()
 
-# шаг-4. формируем датафрейм
-MDF<- data.frame(nr_point, profile1, profile2, profile3, profile4, profile5, profile6, profile7, profile8, profile9, profile10, profile11, profile12, profile13, profile14, profile15, profile16, profile17, profile18, profile19, profile20, profile21, profile22, profile23, profile24, profile25)
-head(MDF)
- 
-# шаг-6. рисуем один график (здесь: профиль №18) по кривой лесса с доверительными интервалами и квантилями
-Loess_profile18 <- ggplot(MDF, aes(x = nr_point, y = profile18)) +
-	geom_point(aes(x = nr_point, y = profile18, colour = "Samples", shape = "Samples"), show.legend=TRUE) +
-	geom_smooth(aes(x = nr_point, y = profile18, colour = "Loess method"), method = loess, se = TRUE, span = .4, size=.3, linetype = "solid", show.legend=TRUE) +
-	geom_smooth(aes(x = nr_point, y = profile18, colour = "Glm method"), method = glm, se = FALSE, span = .4, size=.4, linetype = "dotted", show.legend=TRUE) +
-	geom_smooth(aes(x = nr_point, y = profile18, colour = "Lm method"), method = lm, se = TRUE, size=.3, linetype = "solid", show.legend=TRUE) +
-	geom_quantile(aes(x = nr_point, y = profile18, colour = "Quantiles"), linetype = "solid", show.legend=TRUE) +
+# шаг-3. (здесь: профиль №18)
+Loess_profile18 <- ggplot(MDF, aes(x = observ, y = profile18)) +
+	geom_point(aes(x = observ, y = profile18, colour = "Samples", shape = "Samples"), show.legend=TRUE) +
+	geom_smooth(aes(x = observ, y = profile18, colour = "Loess method"), method = loess, se = TRUE, span = .4, size=.3, linetype = "solid", show.legend=TRUE) +
+	geom_smooth(aes(x = observ, y = profile18, colour = "Glm method"), method = glm, se = FALSE, span = .4, size=.4, linetype = "dotted", show.legend=TRUE) +
+	geom_smooth(aes(x = observ, y = profile18, colour = "Lm method"), method = lm, se = TRUE, size=.3, linetype = "solid", show.legend=TRUE) +
+	geom_quantile(aes(x = observ, y = profile18, colour = "Quantiles"), linetype = "solid", show.legend=TRUE) +
 	xlab("Observations") + 
 	ylab("Depths, m") +
 	scale_color_manual(values = c("Samples" = "seagreen", "Loess method" = "red", "Glm method" = "orange", "Lm method" = "blue", "Quantiles" = "purple")) + # задаем значения цветов элементов
